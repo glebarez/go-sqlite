a0ae409cb1d614ceeefe7c7fbed576e332fba262
Alexander Menzhinsky <amenzhinsky@gmail.com>
2017-05-01 23:49:25 +0300

    Add named parameters support

---
+++ sqlite_go18.go
@@ -0,0 +1,72 @@
+// Copyright 2017 The Sqlite Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build go1.8
+
+package sqlite
+
+import (
+	"context"
+	"database/sql/driver"
+	"errors"
+)
+
+// Ping implements driver.Pinger
+func (c *conn) Ping(ctx context.Context) error {
+	c.Lock()
+	defer c.Unlock()
+
+	if c.ppdb == 0 {
+		return errors.New("db is closed")
+	}
+
+	_, err := c.ExecContext(ctx, "select 1", nil)
+	return err
+}
+
+// BeginTx implements driver.ConnBeginTx
+func (c *conn) BeginTx(ctx context.Context, opts driver.TxOptions) (driver.Tx, error) {
+	return c.begin(ctx, txOptions{
+		Isolation: int(opts.Isolation),
+		ReadOnly:  opts.ReadOnly,
+	})
+}
+
+// PrepareContext implements driver.ConnPrepareContext
+func (c *conn) PrepareContext(ctx context.Context, query string) (driver.Stmt, error) {
+	return c.prepare(ctx, query)
+}
+
+// ExecContext implements driver.ExecerContext
+func (c *conn) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
+	return c.exec(ctx, query, toNamedValues2(args))
+}
+
+// QueryContext implements driver.QueryerContext
+func (c *conn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
+	return c.query(ctx, query, toNamedValues2(args))
+}
+
+// ExecContext implements driver.StmtExecContext
+func (s *stmt) ExecContext(ctx context.Context, args []driver.NamedValue) (driver.Result, error) {
+	return s.exec(ctx, toNamedValues2(args))
+}
+
+// QueryContext implements driver.StmtQueryContext
+func (s *stmt) QueryContext(ctx context.Context, args []driver.NamedValue) (driver.Rows, error) {
+	return s.query(ctx, toNamedValues2(args))
+}
+
+// converts []driver.NamedValue to []namedValue
+func toNamedValues2(vals []driver.NamedValue) []namedValue {
+	args := make([]namedValue, 0, len(vals))
+	for _, val := range vals {
+		args = append(args, namedValue{
+			Name:    val.Name,
+			Ordinal: val.Ordinal,
+			Value:   val.Value,
+		})
+	}
+	return args
+}
---
+++ sqlite_go18_test.go
@@ -0,0 +1,48 @@
+// Copyright 2017 The Sqlite Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build go1.8
+
+package sqlite
+
+import (
+	"database/sql"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestNamedParameters(t *testing.T) {
+	dir, db := tempDB(t)
+	defer func() {
+		db.Close()
+		os.RemoveAll(dir)
+	}()
+
+	_, err := db.Exec(`
+	create table t(s1 varchar(32), s2 varchar(32), s3 varchar(32), s4 varchar(32));
+	insert into t values(?, @aa, $aa, @bb);
+	`, "1", sql.Named("aa", "one"), sql.Named("bb", "two"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rows, err := db.Query("select * from t")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := make([]string, 4)
+	for rows.Next() {
+		if err := rows.Scan(&rec[0], &rec[1], &rec[2], &rec[3]); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	w := []string{"1", "one", "one", "two"}
+	if !reflect.DeepEqual(rec, w) {
+		t.Fatal(rec, w)
+	}
+}
