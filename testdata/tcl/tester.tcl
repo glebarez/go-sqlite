6f3d099498c5bb4e0fcec025878dc741851556d3
Jason DeBettencourt <jason@tashtegollc.com>
2021-03-14 12:07:03 -0400

    merged, re-fixed windows get_pwd.

--- testdata/tcl/tester.tcl
+++ testdata/tcl/tester.tcl
@@ -181,7 +181,7 @@ proc get_pwd {} {
       set comSpec {C:\Windows\system32\cmd.exe}
     }
     return [string map [list \\ /] \
-        [string trim [exec -- $comSpec /c echo %CD%]]]
+        [string trim [exec -- $comSpec /c echo cd ]]]
   } else {
     return [pwd]
   }
