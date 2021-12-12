5e37faf4f1d1a8b294553c499a43b3ff1c841a4d
glebarez <gleb.sakhnov@gmail.com>
2021-12-12 11:54:46 +0100

    remove Tcl testing suite (this is tested in upstream)

--- internal/libc2/capi_windows_amd64.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 The Sqlite Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package libc2 // import "modernc.org/sqlite/internal/libc2"
-
-var CAPI = map[string]struct{}{}
